d1545d8b333d2f27e8ec49a39a81d9faf83bf15d
jeeva jeeva <rishivickey7@gmail.com>
2021-06-16 11:56:47 +0530

    Added Catagory Model

--- BulkuBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkuBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BulkyBook.Model.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace BulkyBook.Data
             : base(options)
         {
         }
+
+        public DbSet<Catagory> Catagories { get; set; }
     }
 }
--- BulkyBook.Model/Models/Catagory.cs
+++ BulkyBook.Model/Models/Catagory.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBook.Model.Models
 {
-   public class Catagory
-    {
+    public class Catagory
+    { 
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
